747d0d9d95eb0069a55a957c6af3374559567799
abe <abram.handler@gmail.com>
2025-05-18 02:31:44 +0000

    k

--- go.sh
+++ go.sh
@@ -1,7 +1,6 @@
 
 #python src/import_huggingface_model.py --model="openlm-research/open_llama_3b" --write_dir="pretrained"
-#python download_data.py
 
-
+python download_data.py
 python src/tokenize_data.py --data_dir="./data" --path_to_tokenizer="./pretrained/tokenizers/open_llama_3b" --path_to_dataset=data/blockeddocs --nb_workers=4
 python src/tokenize_data.py --data_dir="./data" --path_to_tokenizer="./pretrained/tokenizers/open_llama_3b" --path_to_dataset=data/project-gutenberg-extended --nb_workers=4
