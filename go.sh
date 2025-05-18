c3e76926d53d688a9c21511acda6eb7018556ec0
abe <abram.handler@gmail.com>
2025-05-18 01:43:18 +0000

    k

--- go.sh
+++ go.sh
@@ -1,4 +1,7 @@
 
-python src/import_huggingface_model.py --model="openlm-research/open_llama_3b" --write_dir="pretrained"
-python download_data.py
+#python src/import_huggingface_model.py --model="openlm-research/open_llama_3b" --write_dir="pretrained"
+#python download_data.py
+
+ds=$(find datacache/ -type d| grep deepmind___pg19 | grep default/0.1.0/ | head  -1)
+python src/tokenize_data.py --data_dir="./data" --path_to_tokenizer="./pretrained/tokenizers/open_llama_3b" --path_to_dataset=$ds --nb_workers=4
 
