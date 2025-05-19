ee73c691b0aaf9288e05c9392f00c9f509c71277
abe <abram.handler@gmail.com>
2025-05-19 14:36:39 +0000

    k

--- scripts/compute_perplexity.sh
+++ scripts/compute_perplexity.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+export CUDA_VISIBLE_DEVICES=3
+
 for chunk in 0 1 2 3 4
 do
-    CUDA_VISIBLE_DEVICES=0 python src/compute_perplexity.py --data_dir='./data' \
+    CUDA_VISIBLE_DEVICES=3 python src/compute_perplexity.py --data_dir='./data' \
                     --path_to_tokenizer='./pretrained/tokenizers/open_llama_3b' --path_to_model='./pretrained/models/open_llama_3b' \
                     --path_to_dataset="./data/final_chunks/gutenberg_${chunk}_min_tokens100_seed42" \
                     --results_dir='./perplexity_results' --nb_samples=400 --stride=127 --max_length=128  \
