306e92a93344addfb2b0d48fd78962013466b42a
abe <abram.handler@gmail.com>
2025-05-23 01:16:11 +0000

    k

--- src/compute_perplexity.py
+++ src/compute_perplexity.py
@@ -127,7 +127,7 @@ def main():
             indices = pickle.load(f)
             tokenized_dataset = tokenized_dataset.select(indices)
             
-    model = AutoModelForCausalLM.from_pretrained(PATH_TO_MODEL, torch_dtype=torch.float16).to(device)
+    model = AutoModelForCausalLM.from_pretrained(PATH_TO_MODEL, torch_dtype=torch.float32).to(device)
     max_length = args.max_length
     stride = args.stride
     top_probas = args.top_probas
