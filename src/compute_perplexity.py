1b219f97ff08595f47283289446a7e6e974de827
abe <abram.handler@gmail.com>
2025-05-23 03:10:04 +0000

    k

--- src/compute_perplexity.py
+++ src/compute_perplexity.py
@@ -63,11 +63,6 @@ def compute_perplexity(all_tokens_one_doc: list, model: AutoModelForCausalLM,
         loss = F.cross_entropy(shift_logits, shift_targets.view(-1), reduction='none')
         loss_list = list(loss.detach().cpu().numpy())
         if np.isnan(loss_list).any():
-            count_nan = int(np.isnan(loss_list).sum())
-            frac_nan = count_nan/len(loss_list)
-            np_array = input_ids.cpu().numpy()
-            np.save("ids.npy", np_array)
-            import sys; sys.exit(0)
             print(f"[WARN] NaN in loss_list at doc slice {begin_loc}:{end_loc}, frac nan is {frac_nan:.2%}")
 
         # convert to probas
