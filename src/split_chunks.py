f9a7db974da56a11dcb2b6379c9c145c3a5b55c7
Your Name <abha4861@cu-vpn-leeds-172.21.36.37.int.colorado.edu>
2025-05-30 12:37:13 -0600

    adjusting sto store meta data information, and removing some code that we are not using

--- src/split_chunks.py
+++ src/split_chunks.py
@@ -4,6 +4,7 @@ import pickle
 from datasets import load_from_disk, concatenate_datasets
 from tqdm import tqdm
 import random 
+import json
 
 def get_parser():
     """Return parser for the args."""
@@ -17,12 +18,15 @@ def get_parser():
     parser.add_argument("--path_to_member_data", type=str, required=True)
     parser.add_argument("--path_to_non_member_data", type=str, required=True)
     parser.add_argument("--min_tokens", type=int, default=0)
-    parser.add_argument("--filter_on_date", type=int, default=0)
+    #parser.add_argument("--filter_on_date", type=int, default=0)
     parser.add_argument("--n_chunks", type=int, default=5)
     parser.add_argument("--n_pos_chunk", type=int, default=200)
     parser.add_argument("--seed", type=int, default=42)
 
     return parser
+
+'''
+We are not using this code so commenting out
 
 def filter_on_date(dataset, date_name, min_date=1850, max_date=1910):
     all_indices = range(len(dataset))
@@ -61,6 +65,7 @@ def remove_small_docs(dataset, min_tokens):
     print("Number of documents after min token filtering: ", len(sub_dataset))
 
     return sub_dataset
+'''
 
 def main(args):
     """Main function to call."""
@@ -69,21 +74,24 @@ def main(args):
     
     print("Loading the raw data..")
     og_member_dataset = load_from_disk(args.path_to_member_data)
-    if args.filter_on_date:
-        og_member_dataset = filter_on_date(og_member_dataset, date_name="publication_date")
+    #if args.filter_on_date:
+    #    og_member_dataset = filter_on_date(og_member_dataset, date_name="publication_date")
     og_member_dataset = og_member_dataset.select_columns(['input_ids', 'attention_mask'])
     og_non_member_dataset = load_from_disk(args.path_to_non_member_data)
-    if args.filter_on_date:
-        og_non_member_dataset = filter_on_date(og_non_member_dataset, date_name="original_publication")
+    
+    #if args.filter_on_date:
+    #    og_non_member_dataset = filter_on_date(og_non_member_dataset, date_name="original_publication")
     og_non_member_dataset = og_non_member_dataset.select_columns(['input_ids', 'attention_mask'])
 
-    if args.min_tokens > 0:
-        print("Removing the docs with limited tokens..")
-        member_dataset = remove_small_docs(og_member_dataset, args.min_tokens)
-        non_member_dataset = remove_small_docs(og_non_member_dataset, args.min_tokens)
-    else:
-        member_dataset = og_member_dataset
-        non_member_dataset = og_non_member_dataset
+    #if args.min_tokens > 0:
+    #    print("Removing the docs with limited tokens..")
+    #    member_dataset = remove_small_docs(og_member_dataset, args.min_tokens)
+    #    non_member_dataset = remove_small_docs(og_non_member_dataset, args.min_tokens)
+    #else:
+
+    # we are not using this code.
+    member_dataset = og_member_dataset
+    non_member_dataset = og_non_member_dataset
 
     print("Composing the chunks...")
     members_samples_idx = random.sample(range(len(member_dataset)), args.n_chunks * args.n_pos_chunk)
@@ -92,6 +100,17 @@ def main(args):
     for chunk_id in range(args.n_chunks):
         chunk_members = member_dataset.select(members_samples_idx[chunk_id * args.n_pos_chunk : (chunk_id + 1) * args.n_pos_chunk])
         chunk_non_members = non_member_dataset.select(non_members_samples_idx[chunk_id * args.n_pos_chunk : (chunk_id + 1) * args.n_pos_chunk])
+        
+        chunk_members_metadata = {k: chunk_members[k] for k in ["id", "url"]}
+        chunk_non_members_metadata = {k: chunk_non_members[k] for k in ["id", "url"]}
+
+        # we need to store the metadata information for each chunk to link scores w/ downstream analysis
+        with open(f"{args.output_dir}/{args.prefix}_{chunk_id}_metadata.json", "w") as f:
+            json.dump({
+                "chunk_members_metadata": chunk_members_metadata,
+                "chunk_non_members_metadata": chunk_non_members_metadata
+            }, f)
+
         chunk_dataset_all = concatenate_datasets([chunk_members, chunk_non_members])
         labels = [1] * len(chunk_members) + [0] * len(chunk_members)
 
