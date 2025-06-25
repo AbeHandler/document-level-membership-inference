536e4499a3e523f535c3a8043931e7082be94d05
Your Name <abha4861@cu-vpn-leeds-172.21.36.17.int.colorado.edu>
2025-06-24 22:49:54 -0600

    k

--- scripts/merge_copyright.py
+++ scripts/merge_copyright.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-blocks = pd.read_csv("classifier_results/chunks/copywritetraps_blockbench-blocksbin_chunkXX.csv").rename(columns={"score": "score_blocks"})
-noblocks = pd.read_csv("classifier_results/chunks/copywritetraps_blockbench-noblocksbin_chunkXX.csv").rename(columns={"score": "score_noblocks"})
 
-print(len(blocks))
-import pdb; pdb.set_trace()
+# TODO this drop dupes could be a little tighter. I am not 100% of the source
+blocks = pd.read_csv("classifier_results/chunks/copywritetraps_blockbench-blocksbin_chunkXX.csv").rename(columns={"score": "score_blocks"}).drop_duplicates(subset="id", keep="first")
+noblocks = pd.read_csv("classifier_results/chunks/copywritetraps_blockbench-noblocksbin_chunkXX.csv").rename(columns={"score": "score_noblocks"}).drop_duplicates(subset="id", keep="first")
+
+
 D = blocks.merge(noblocks,on="id")
 
-print(len(D))
 # returns a prediction for binary membership of input document
 # 4.4 Meta-classifier 
 D["delta"] = D["score_noblocks"] - D["score_blocks"]
