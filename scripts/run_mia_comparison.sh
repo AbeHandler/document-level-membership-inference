4f5e4318315308fae4059ead27fb2ecb2c3fb3e6
Your Name <abha4861@cu-vpn-leeds-172.21.36.117.int.colorado.edu>
2025-07-02 12:53:39 -0600

    k

--- scripts/run_mia_comparison.sh
+++ scripts/run_mia_comparison.sh
@@ -1,1 +1,3 @@
+mkdir data/minhashblocksample_targetsonly_doc_level_mia
 ./go.sh dobolyilab/blockbench-noblocksbin minhashblocksample_targetsonly_doc_level_mia 501 501 5
+./go.sh dobolyilab/blockbench-blocksbin minhashblocksample_targetsonly_doc_level_mia 501 501 5
