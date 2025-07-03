8b9530cc00d5e62c9c68ffb7393de3293de61a23
Your Name <abha4861@cu-vpn-leeds-172.21.36.101.int.colorado.edu>
2025-07-02 21:50:36 -0600

    k

--- go.sh
+++ go.sh
@@ -61,6 +61,7 @@ for chunk in $(seq 0 $((N_CHUNKS - 1))); do
         --path_to_dataset="./data/final_chunks/${CHUNK_PREFIX}_${chunk}_min_tokens100_seed42" \
         --results_dir='./perplexity_results' --stride=127 --max_length=128 \
         --top_probas=10 --shuffle=0 \
+        --nb_samples "$NB_SAMPLES" \
         --general_proba_path="./data/final_chunks/general_proba/general_proba_${CHUNK_PREFIX}_${chunk}_128.pickle" \
         --token_freq_path="./data/final_chunks/token_freq/token_freq_${CHUNK_PREFIX}_${chunk}.pickle"
 done
