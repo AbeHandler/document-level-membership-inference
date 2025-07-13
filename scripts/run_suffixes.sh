ec8081a1c7d521d2f1dc3854375c0eb8cdad28e6
Your Name <abha4861@cu-vpn-leeds-172.21.36.20.int.colorado.edu>
2025-07-12 20:09:50 -0600

    k

--- scripts/run_suffixes.sh
+++ scripts/run_suffixes.sh
@@ -1,2 +1,2 @@
-./go.sh dobolyilab/blockbench-noblocksbin suffixesnoblocksbin 472 472 1
-./go.sh dobolyilab/blockbench-blocksbin suffixesnoblocksbin 472 472 1
+./go.sh dobolyilab/blockbench-noblocksbin suffixesnoblocksbin 236 236 2
+./go.sh dobolyilab/blockbench-blocksbin suffixesnoblocksbin 236 236 2
